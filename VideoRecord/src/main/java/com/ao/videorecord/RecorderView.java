67a6c247624d46ba45c4c8824293a5336a31d73e
xiaxiaoao <xiaye12345@hotmail.com>
2016-10-12 17:17:14 +0800

    inital version
    inital version

---
+++ VideoRecord/src/main/java/com/ao/videorecord/RecorderView.java
@@ -0,0 +1,232 @@
+package com.ao.videorecord;
+
+import android.app.Activity;
+import android.content.Context;
+import android.hardware.Camera;
+import android.media.CamcorderProfile;
+import android.media.MediaRecorder;
+import android.os.Environment;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.util.SparseIntArray;
+import android.view.Surface;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.WindowManager;
+import android.widget.Toast;
+
+import java.io.IOException;
+
+/**
+ * Created by xiaao on 12/10/2016.
+ */
+
+public class RecorderView extends SurfaceView implements SurfaceHolder.Callback {
+
+    public static String TAG = "RecorderView";
+
+    private boolean recording;
+    private MediaRecorder mediaRecorder;
+    private SurfaceHolder surfaceHolder;
+    private String outputFileLocation;
+
+    private Camera camera;
+    private int orientation = 0;
+
+    public RecorderView(Context context) {
+        super(context, null);
+        Log.d(TAG, "super(context, null)");
+    }
+
+    public RecorderView(Context context, AttributeSet attrs) {
+        super(context, attrs, 0);
+        Log.d(TAG, "super(context, attrs, 0)");
+
+        outputFileLocation = context.getExternalFilesDir(null).getAbsolutePath() + "/" + System.currentTimeMillis() + ".mp4";
+
+        recording = false;
+
+        surfaceHolder = getHolder();
+        surfaceHolder.addCallback(this);
+
+        prepareMediaRecorder();
+    }
+
+    private boolean prepareMediaRecorder() {
+        camera = getCameraInstance();
+
+        if (camera == null) {
+            return false;
+        }
+
+        // set the orientation here to enable portrait recording.
+        setCameraDisplayOrientation(0);
+        mediaRecorder = new MediaRecorder();
+        camera.unlock();
+        mediaRecorder.setCamera(camera);
+
+        mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mediaRecorder.setVideoSource(MediaRecorder.VideoSource.DEFAULT);
+
+//        mediaRecorder.setProfile(CamcorderProfile.get(CamcorderProfile.QUALITY_HIGH));
+
+        //set video format
+        mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
+        //set audio format
+        mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AAC);
+        //set bit rate
+        mediaRecorder.setVideoEncodingBitRate(10000000);
+        //set video encoder
+        mediaRecorder.setVideoEncoder(MediaRecorder.VideoEncoder.H264);
+        //set video size
+        mediaRecorder.setVideoSize(1280, 720);
+        //set video frame rate
+        mediaRecorder.setVideoFrameRate(30);
+        //set max duration
+        mediaRecorder.setMaxDuration(10000);
+
+        //set output location
+        mediaRecorder.setOutputFile(outputFileLocation);
+        mediaRecorder.setPreviewDisplay(surfaceHolder.getSurface());
+        mediaRecorder.setOrientationHint(orientation);
+
+        try {
+            mediaRecorder.prepare();
+        } catch (Exception e) {
+            releaseMediaRecorder();
+            return false;
+        }
+        return true;
+
+    }
+
+    private Camera getCameraInstance() {
+        Camera c;
+        try {
+            c = Camera.open(Camera.CameraInfo.CAMERA_FACING_BACK); // attempt to get a Camera instance
+            return c;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        try {
+            camera.setPreviewDisplay(surfaceHolder);
+            camera.startPreview();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+        try {
+            setCameraDisplayOrientation(0);
+            previewCamera();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+
+    }
+
+    //Call when activity pause
+    public void onActivityPause() {
+        releaseMediaRecorder();       // if you are using MediaRecorder, release it first
+        releaseCamera();              // release the camera immediately on pause event
+    }
+
+    public void setOutputFile(String filename) {
+        outputFileLocation = filename;
+        mediaRecorder.setOutputFile(filename);
+    }
+
+    public void startRecording() {
+        releaseCamera();
+        if (!prepareMediaRecorder()) {
+            Toast.makeText(getContext(), "Fail in prepareMediaRecorder()!", Toast.LENGTH_LONG).show();
+            return;
+        }
+        mediaRecorder.start();
+        recording = true;
+    }
+
+    public void stopRecording() {
+        mediaRecorder.stop();  // stop the recording
+        releaseMediaRecorder(); // release the MediaRecorder object
+        Toast.makeText(getContext(), "Video saved:" + outputFileLocation, Toast.LENGTH_SHORT).show();
+        recording = false;
+    }
+
+    public boolean isRecording() {
+        return recording;
+    }
+
+    private void releaseMediaRecorder() {
+        if (mediaRecorder != null) {
+            mediaRecorder.reset();   // clear recorder configuration
+            mediaRecorder.release(); // release the recorder object
+            mediaRecorder = new MediaRecorder();
+            camera.lock();           // lock camera for later use
+        }
+    }
+
+    private void releaseCamera() {
+        if (camera != null) {
+            camera.release();        // release the camera for other applications
+            camera = null;
+        }
+    }
+
+    public void previewCamera() {
+        try {
+            camera.setPreviewDisplay(surfaceHolder);
+            camera.startPreview();
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.d(TAG, "Cannot start preview", e);
+        }
+    }
+
+    public void setCameraDisplayOrientation(int cameraId) {
+
+        android.hardware.Camera.CameraInfo info = new android.hardware.Camera.CameraInfo();
+        android.hardware.Camera.getCameraInfo(cameraId, info);
+
+        int rotation = ((Activity) getContext()).getWindowManager().getDefaultDisplay().getRotation();
+        int degrees = 0;
+
+        switch (rotation) {
+            case Surface.ROTATION_0:
+                degrees = 0;
+                break;
+            case Surface.ROTATION_90:
+                degrees = 90;
+                break;
+            case Surface.ROTATION_180:
+                degrees = 180;
+                break;
+            case Surface.ROTATION_270:
+                degrees = 270;
+                break;
+        }
+
+        int result;
+        if (info.facing == Camera.CameraInfo.CAMERA_FACING_FRONT) {
+            result = (info.orientation + degrees) % 360;
+            result = (360 - result) % 360;  // compensate the mirror
+        } else {  // back-facing
+            result = (info.orientation - degrees + 360) % 360;
+        }
+        orientation = result;
+        camera.setDisplayOrientation(result);
+    }
+
+}
---
+++ app/src/main/java/com/ao/videorecorder/MainActivity.java
@@ -0,0 +1,134 @@
+package com.ao.videorecorder;
+
+import android.Manifest;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import com.ao.videorecord.RecorderView;
+import com.ao.videorecorder.dialog.ErrorDialog;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String[] VIDEO_PERMISSIONS = {
+            Manifest.permission.CAMERA,
+            Manifest.permission.RECORD_AUDIO,
+    };
+    private static final int REQUEST_VIDEO_PERMISSIONS = 1;
+
+    private RecorderView recorderView;
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        recorderView = (RecorderView) findViewById(R.id.recorderView);
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!hasPermissionsGranted(VIDEO_PERMISSIONS)) {
+                    requestVideoPermissions();
+                    return;
+                }
+                if (recorderView.isRecording()) {
+                    recorderView.stopRecording();
+                } else {
+                    recorderView.startRecording();
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        recorderView.onActivityPause();
+    }
+
+    private boolean shouldShowRequestPermissionRationale(String[] permissions) {
+        for (String permission : permissions) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, permission)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Requests permissions needed for recording video.
+     */
+    private void requestVideoPermissions() {
+        if (shouldShowRequestPermissionRationale(VIDEO_PERMISSIONS)) {
+            new ConfirmationDialog().show(getFragmentManager(), "Request");
+        } else {
+            ActivityCompat.requestPermissions(this, VIDEO_PERMISSIONS, REQUEST_VIDEO_PERMISSIONS);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_VIDEO_PERMISSIONS) {
+            if (grantResults.length == VIDEO_PERMISSIONS.length) {
+                for (int result : grantResults) {
+                    if (result != PackageManager.PERMISSION_GRANTED) {
+                        ErrorDialog.newInstance(getString(R.string.permission_request))
+                                .show(getFragmentManager(), "");
+                        break;
+                    }
+                }
+            } else {
+                ErrorDialog.newInstance(getString(R.string.permission_request))
+                        .show(getFragmentManager(), "ErrorDialog");
+            }
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+    private boolean hasPermissionsGranted(String[] permissions) {
+        for (String permission : permissions) {
+            if (ActivityCompat.checkSelfPermission(this, permission)
+                    != PackageManager.PERMISSION_GRANTED) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static class ConfirmationDialog extends DialogFragment {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            return new AlertDialog.Builder(getActivity())
+                    .setMessage(R.string.permission_request)
+                    .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            ActivityCompat.requestPermissions(getActivity(), VIDEO_PERMISSIONS,
+                                    REQUEST_VIDEO_PERMISSIONS);
+                        }
+                    })
+                    .setNegativeButton(android.R.string.cancel,
+                            new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    getActivity().finish();
+                                }
+                            })
+                    .create();
+        }
+
+    }
+
+
+}
